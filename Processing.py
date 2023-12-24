fd50a8b4574a39c1a43aff0e73513f12b78f6434
Bagus Rahma AC <46398272+isNothingSpecial@users.noreply.github.com>
2023-12-24 17:14:07 +0700

    Update Processing.py

--- Processing.py
+++ Processing.py
@@ -1,16 +1,7 @@
 import streamlit as st
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-from sklearn.feature_extraction.text import TfidfVectorizer
 from textblob import TextBlob
-import re
-import nltk
-nltk.download('punkt')
-nltk.download('stopwords')
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
+import matplotlib.pyplot as plt
 
 # Function to analyze sentiment
 def analyze_sentiment(text):
@@ -23,21 +14,7 @@ def analyze_sentiment(text):
     else:
         return "Netral"
 
-# Function to preprocess text
-def preprocessing(text):
-    text = str(text)
-    text = text.lower()
-    text = re.sub(r'\d+', '', text)
-    text = re.sub(r'[^\w\s]', '', text)
-    text = text.strip()
-    tokens = nltk.word_tokenize(text)
-    stop_words = set(stopwords.words('indonesian'))
-    filtered_tokens = [token for token in tokens if token not in stop_words]
-    stemmer = PorterStemmer()
-    stemmed_tokens = [stemmer.stem(token) for token in filtered_tokens]
-    return ' '.join(stemmed_tokens)
-
-# Streamlit App
+# Main function
 def main():
     st.title("Aplikasi Sentiment Analysis dengan Dataset CSV")
 
@@ -61,36 +38,10 @@ def main():
                 st.write("Hasil Analisis Sentimen:")
                 st.write(df)
                 
-                # Create a bar chart using Streamlit
-                st.write("Visualisasi Rekap Hasil Analisis:")
+                # Create a line chart using Matplotlib
+                st.write("Visalisasi Rekap Hasil Analisis:")
                 sentiment_counts = df['Sentiment'].value_counts()
-                st.bar_chart(sentiment_counts)  # This will create a bar chart from sentiment counts
-                
-                # Preprocess data for classification
-                X = df[text_column].apply(preprocessing)
-                Y = df['sentimen']
-
-                vectorizer = TfidfVectorizer()
-                X = vectorizer.fit_transform(X)
-
-                X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
-
-                knn = KNeighborsClassifier(n_neighbors=5)
-                knn.fit(X_train, Y_train)
-
-                y_pred = knn.predict(X_test)
-
-                # Evaluation metrics
-                accuracy = accuracy_score(Y_test, y_pred)
-                precision = precision_score(Y_test, y_pred, average='weighted')
-                recall = recall_score(Y_test, y_pred, average='weighted')
-                f1 = f1_score(Y_test, y_pred, average='weighted')
-
-                st.write('Evaluasi Model:')
-                st.write(f'Accuracy: {accuracy}')
-                st.write(f'Precision: {precision}')
-                st.write(f'Recall: {recall}')
-                st.write(f'F1 Score: {f1}')
+                st.pie_chart(sentiment_counts)  # This will create a line chart from sentiment counts
             else:
                 st.write("Pilih kolom teks terlebih dahulu.")
 
