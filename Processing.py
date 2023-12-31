2a5507259da0136bd20384cc160ea9d99f426373
Bagus Rahma AC <46398272+isNothingSpecial@users.noreply.github.com>
2023-12-31 09:11:45 +0700

    Update Processing.py

--- Processing.py
+++ Processing.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from textblob import TextBlob
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Function to analyze sentiment
 def analyze_sentiment(text):
@@ -16,37 +17,46 @@ def analyze_sentiment(text):
 
 # Main function
 def main():
-    st.title("Aplikasi Sentiment Analysis dengan Dataset CSV")
+    st.title("Aplikasi Sentiment Analysis dengan Dataset CSV atau Excel")
 
-    # Upload CSV file
-    uploaded_file = st.file_uploader("Upload file CSV", type=["csv"])
+    # Upload file
+    uploaded_file = st.file_uploader("Upload file CSV atau Excel", type=["csv", "xlsx"])
 
     if uploaded_file is not None:
-        # Read the CSV file
-        df = pd.read_csv(uploaded_file)
-
-        st.write("Data yang Diunggah:")
-        st.write(df)
-
-        # Select column containing text data
-        text_column = st.selectbox("Pilih kolom teks:", df.columns)
-
-        # Analyze sentiment for selected text column
-        if st.button("Analyze Sentiment"):
-            if text_column:
-                df['Sentiment'] = df[text_column].apply(analyze_sentiment)
-                st.write("Hasil Analisis Sentimen:")
-                st.write(df)
-                
-                # Create a line chart using Matplotlib
-                st.write("Visalisasi Rekap Hasil Analisis:")
-                sentiment_counts = df['Sentiment'].value_counts()
-                fig, ax = plt.subplots()
-                ax.pie(sentiment_counts, labels=sentiment_counts.index, autopct='%1.1f%%', startangle=90)
-                ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-                st.pyplot(fig)  # Display the Matplotlib plot in Streamlit
+        try:
+            # Read the file
+            if uploaded_file.type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
+                df = pd.read_excel(uploaded_file)
             else:
-                st.write("Pilih kolom teks terlebih dahulu.")
+                df = pd.read_csv(uploaded_file)
+
+            st.write("Data yang Diunggah:")
+            st.write(df)
+
+            # Select column containing text data
+            text_column = st.selectbox("Pilih kolom teks:", df.columns)
+
+            # Analyze sentiment for selected text column
+            if st.button("Analyze Sentiment"):
+                if text_column:
+                    df['Sentiment'] = df[text_column].apply(analyze_sentiment)
+                    st.write("Hasil Analisis Sentimen:")
+                    st.write(df)
+
+                    # Visualization - Bar Chart
+                    sentiment_counts = df['Sentiment'].value_counts()
+                    plt.figure(figsize=(8, 6))
+                    sns.barplot(x=sentiment_counts.index, y=sentiment_counts.values)
+                    plt.title('Distribusi Sentimen')
+                    plt.xlabel('Sentimen')
+                    plt.ylabel('Jumlah')
+                    st.pyplot()
+
+                else:
+                    st.write("Pilih kolom teks terlebih dahulu.")
+
+        except Exception as e:
+            st.write("Terjadi kesalahan:", e)
 
 if __name__ == "__main__":
     main()
